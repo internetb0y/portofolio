b5d9af6e997266ede247ba7192ffbe1ef403da25
Aji Wicaksana <1.internetboy.0@gmail.com>
2024-03-29 21:46:29 +0700

    Update feature dialog box

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
     socialBtn.addEventListener('click', () => {
         document.querySelector('.children-social').classList.toggle('show');
     })
+
+    const popup = document.querySelector('#popup');
+
+    setTimeout(() => {
+        popup.showModal();
+
+        const close = document.querySelector('#close');
+
+        close.addEventListener('click', () => {
+            popup.close();
+        })
+    }, 0);
 })
