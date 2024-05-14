ac5bc2b5a9d4d435871a3a48bfed0bbd72b00e46
Aji Wicaksana <1.internetboy.0@gmail.com>
2024-05-14 09:21:31 +0700

    "Update index.html, script.js"

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 // Update This File
-const greetingsModal = () => {
-    setTimeout(() => {
-        document.querySelector('#popup').showModal();
-
-        const close = document.querySelector('#close');
-        close.addEventListener('click', () => popup.close());
-    }, 0);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    greetingsModal();
-
-    const socialBtn = document.querySelector('#social');
+        const socialBtn = document.querySelector('#social');
     socialBtn.addEventListener('click', () => document.querySelector('.children-social').classList.toggle('show'));
 })
